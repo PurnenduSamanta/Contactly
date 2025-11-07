f068f484498153ecc62982e16f251a24710b3450
PurnenduSamanta <joysamanta123@gmail.com>
2025-11-07 12:04:40 +0530

    5th_commit_07_11_2025

--- app/src/main/java/com/purnendu/contactly/ui/screens/SchedulesScreen.kt
+++ app/src/main/java/com/purnendu/contactly/ui/screens/SchedulesScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Icon
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
