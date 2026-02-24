66a0831ed08e65a7560eaf5dec96ee5501625cc9
PurnenduSamanta <joysamanta123@gmail.com>
2026-02-24 20:15:04 +0530

    144th_24_02_2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.purnendu.contactly"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "3.3"
+        versionCode = 7
+        versionName = "3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
